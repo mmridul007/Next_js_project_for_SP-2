653f7d502597fbe9fdf9b08c0b569f2eaa0319eb
Abdullah Al Rafi <100336287+rafi585@users.noreply.github.com>
2022-12-02 23:46:20 +0600

    Rafibro

--- dizme/src/components/About.js
+++ dizme/src/components/About.js
@@ -11,24 +11,24 @@ const About = ({ dark }) => {
                 <div className="numbers year">
                   <div className="wrapper">
                     <h3>
-                      <Counter end={18} />
+                      <Counter end={27} />
                     </h3>
                     <span className="name">
                       Years of
                       <br />
-                      Success
+                      Research
                     </span>
                   </div>
                 </div>
                 <div className="numbers project">
                   <div className="wrapper">
                     <h3>
-                      <Counter end={9} />K
+                      <Counter end={46} />
                     </h3>
                     <span className="name">
-                      Total
+                      I have completed
                       <br />
-                      Projects
+                      Projects and continue...
                     </span>
                   </div>
                 </div>
@@ -36,17 +36,21 @@ const About = ({ dark }) => {
             </div>
             <div className="right">
               <div className="title wow fadeInUp" data-wow-duration="1s">
-                <span>{`I'm a Designer`}</span>
-                <h3>I Can Design Anything You Want</h3>
+                <span>{`I am  the Ralph and Catherine Fisher Professor of Computer Science at the  University of Illinoisat Urbana – Champaign ,and directs the Coordinated Science Laboratory there. Her research concerns , multimedia ,quality of service and middleware`}</span>
+                <h3>You can learn anything form me </h3>
               </div>
               <div className="text wow fadeInUp" data-wow-duration="1s">
                 <p>
-                  {`Hello there! I'm a web designer, and I'm very passionate and
-                  dedicated to my work. With 20 years experience as a
-                  professional web developer, I have acquired the skills and
-                  knowledge necessary to make your project a success. I enjoy
-                  every step of the design process, from discussion and
-                  collaboration.`}
+                  {`  Multimedia operating systems.
+
+Multimedia communication protocols.
+ QoS middleware and large scale distributed systems.
+
+Multimedia security and trustworthy computing systems.
+
+ Advanced tele-immersive and multimedia applications.
+
+High speed QoS routing and ad hoc networks`}
                 </p>
               </div>
               <div
@@ -54,7 +58,7 @@ const About = ({ dark }) => {
                 data-wow-duration="1s"
               >
                 <a className="anchor" href="#contact">
-                  <span>Hire Me</span>
+                  <span>Mail Me</span>
                 </a>
               </div>
             </div>
@@ -71,3 +75,4 @@ const About = ({ dark }) => {
   );
 };
 export default About;
+
